03224f07b684384dd206fe51557868e08792c309
Shanto Miah <web.dev.shanto@gmail.com>
2021-07-12 09:10:59 +0600

    Fix app crash after minify

--- app/src/main/java/com/shanto/miah/wm/app/data/entitys/Word.kt
+++ app/src/main/java/com/shanto/miah/wm/app/data/entitys/Word.kt
@@ -1,12 +1,14 @@
 package com.shanto.miah.wm.app.data.entitys
 
 import android.os.Parcelable
+import androidx.annotation.Keep
 import androidx.room.Entity
 import androidx.room.PrimaryKey
 import kotlinx.android.parcel.Parcelize
 
+@Keep
 @Parcelize
-@Entity(tableName = "word")
+@Entity()
 data class Word(
     @PrimaryKey(autoGenerate = true)
     val id: Long,
